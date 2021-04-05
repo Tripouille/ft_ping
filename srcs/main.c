e80a66fc3949eb2a0f65d027e6bcf04d7ed5b71a
Tripouille <jmichel.gambard@gmail.com>
2021-04-05 16:06:13 -0400

    setup done.

---
+++ srcs/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main(void) {
+	printf("coucou");
+	return (0);
+}
+
+/*
+◦ getpid.
+◦ getuid.
+◦ getaddrinfo.
+◦ gettimeofday.
+◦ inet_ntop.
+◦ inet_pton
+◦ exit.
+◦ signal.
+◦ alarm.
+◦ setsockopt.
+◦ recvmsg.
+◦ sendto.
+◦ socket.
+◦ les fonctions de la famille printf.
+◦ Vous avez l’autorisation d’utiliser d’autres fonctions dans le cadre de vos bonus,
+à condition que leur utilisation soit dûment justifiée lors de votre correction.
+Soyez malins.
+*/
